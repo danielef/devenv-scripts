f943b051f2ce4ccf13faad802e36897d30a5d740
Daniel Estevez <daniel.ef@gmail.com>
2018-09-13 22:21:54 -0500

    Adding emacs configs

---
+++ .emacs.d/elpa/dash-20180413.30/dash-autoloads.el
@@ -0,0 +1,16 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("dash-pkg.el" "dash.el") (23253 13568
+;;;;;;  796511 523000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-autoloads.el ends here
---
+++ .emacs.d/elpa/magit-popup-20180329.1302/magit-popup-autoloads.el
@@ -0,0 +1,16 @@
+;;; magit-popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("magit-popup-pkg.el" "magit-popup.el")
+;;;;;;  (23253 13554 597476 611000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; magit-popup-autoloads.el ends here
