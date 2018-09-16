ece2edc90a95f9f35c637ef0e93d5014e77614e4
Daniel Estevez <daniel.ef@gmail.com>
2018-09-15 20:40:04 -0500

    Adding Homebrew utils

---
+++ brew-install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin
+
+# Installing brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install emacs --with-cocoa
+brew install ImageMagick
+brew install python3
+brew install pygmentize
+brew install leiningen
+brew install htop
+brew install tree
+brew install watch
