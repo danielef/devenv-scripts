61ac0ecd7df2c2b0c368088c74c7e52c745f0856
Daniel Estevez Fuentes <danielef@Daniels-MacBook-Pro.local>
2020-09-26 14:45:33 -0500

    Removes emacs cocoa

--- brew-install.sh
+++ brew-install.sh
@@ -3,10 +3,9 @@
 # Installing brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install emacs --with-cocoa
+brew install emacs
 brew install ImageMagick
 brew install python3
-brew install pygmentize
 brew install leiningen
 brew install htop
 brew install tree
