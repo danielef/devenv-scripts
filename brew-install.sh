959d86ba22c02b08eac23bfbad98f2ecae055f2d
Daniel Estevez <daniel.ef@gmail.com>
2020-09-27 16:15:39 -0500

    Fix emacs to cask

--- brew-install.sh
+++ brew-install.sh
@@ -3,7 +3,7 @@
 # Installing brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install emacs
+brew cask install emacs
 brew install ImageMagick
 brew install python3
 brew install pygmentize
