4aa2375bffa64dd0e71fd6957ac702c4e6068d95
Daniel Estévez <daniel.ef@gmail.com>
2021-01-18 21:06:40 -0600

    Update brew-install.sh

--- brew-install.sh
+++ brew-install.sh
@@ -3,19 +3,22 @@
 # Installing brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew cask install emacs
-brew install ImageMagick
-brew install python3
-brew install pygmentize
-brew install leiningen
-brew install htop
-brew install tree
-brew install watch
-brew install wget
-
+# Terminal
 brew cask install iterm2
 
 # Install openjdk
 brew tap adoptopenjdk/openjdk
 brew cask install adoptopenjdk8
 brew cask install adoptopenjdk11
+
+brew cask install emacs
+brew install ImageMagick
+brew install python3
+brew install leiningen
+brew install htop
+brew install tree
+brew install watch
+brew install wget
+
+# Python deps
+pip3 install pygmentize
