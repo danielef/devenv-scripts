4c46fd867d703b6975d15a8b5e71ae2f9cc9ca43
Daniel Estévez <daniel.ef@gmail.com>
2026-02-16 21:25:53 -0600

    Update brew-install.sh for new installations
    Updated Homebrew installation script to use the new install method and changed JDK version. Added Visual Studio Code installation and commented out pygments and ffmpeg installations.

--- brew-install.sh
+++ brew-install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Installing brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Terminal
 brew install --cask iterm2
 
 # Install openjdk
 brew tap adoptopenjdk/openjdk
-brew install --cask adoptopenjdk8
-brew install --cask adoptopenjdk11
+brew install --cask adoptopenjdk25
 brew install clojure/tools/clojure
 
+brew install --cask visual-studio-code
 brew install --cask emacs
 # Fix font rendering
 defaults write org.gnu.Emacs AppleFontSmoothing -int 0
@@ -24,6 +24,6 @@ brew install tree
 brew install watch
 brew install wget
 brew install nvm
-brew install pygments
-brew install ffmpeg
+# brew install pygments
+# brew install ffmpeg
 
